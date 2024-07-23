5a15a11f0891bc2713b1f2855882618eba1bc8fd
Ramil Sagidullin <rasagidullin@avito.ru>
2024-07-23 22:42:48 +0300

    feat: removed unnecessary deps

--- src/components/index.ts
+++ src/components/index.ts
@@ -12,7 +12,6 @@ export { ThemeToggle } from './ThemeToggle';
 export { GenerationList } from './GenerationList';
 export { Markdown } from './Markdown';
 export { GenerationView } from './GenerationView';
-export { MarkdownEditor } from './MarkdownEditor';
 export { Features } from './Features';
 export { Tariffs } from './Tariffs';
 export { PaymentModal } from './PaymentModal';
