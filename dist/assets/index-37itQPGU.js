afa2059cfc59d14e76b1285bff3ed5f0d9fd3490
Ramil Sagidullin <rasagidullin@avito.ru>
2024-08-15 12:45:30 +0300

    feat: build

--- dist/assets/index-37itQPGU.js
+++ dist/assets/index-37itQPGU.js
@@ -1062,6 +1062,10 @@ In order to be iterable, non-array objects must have a [Symbol.iterator]() metho
   padding-top: 25px;
   padding-bottom: 25px;
 
+  hr {
+    display: none;
+  }
+
   &::-webkit-scrollbar {
     display: none;
   }
